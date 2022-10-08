2cbd4f66d3066636b2df9419f2d617ad3dc8cb96
1152256748@qq.com <1152256748@qq.com>
2022-10-08 23:21:35 +0800

    first commit

---
+++ p2system/src/main.js
@@ -0,0 +1,22 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementPlus from 'element-plus'
+import { Button,Form, Field, CellGroup,NavBar } from 'vant'
+import 'element-plus/dist/index.css'
+import '@/assets/css/global.css'
+import '@element-plus/icons/lib'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import "amfe-flexible"
+
+createApp(App).use(store)
+    .use(router)
+    .use(Button)
+    .use(Form)
+    .use(Field)
+    .use(CellGroup)
+    .use(NavBar)
+    .use(ElementPlus,{
+        locale: zhCn,
+    }).mount('#app')
