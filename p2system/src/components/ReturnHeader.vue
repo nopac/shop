328d3d780c0ff83411a446968e668125733e86c0
ntt-TNT <ntt010910@icloud.com>
2022-10-15 17:02:36 +0800

    GoodDetails.vue Purchase.vue

--- p2system/src/components/ReturnHeader.vue
+++ p2system/src/components/ReturnHeader.vue
@@ -1,43 +1,23 @@
 <template>
-  <div style="height: 50px; line-height: 50px; border-bottom: 1px solid #ccc; display: flex;">
 
-    <div style="width: 200px; padding-left: 30px; font-weight: bold; color: dodgerblue;cursor: pointer" @click="returnGo">
+  <div>
+    <van-nav-bar
+        title=""
+        left-text="返回"
+        left-arrow
+        @click-left="returnGo"
+    />
 
-      <el-icon style="position: relative;top: 3px" size="large"><ArrowLeftBold /></el-icon>
-      <span>返回</span>
-    </div>
-
-    <div style="flex: 1"></div>
-    <div style="width: 100px">
-      <el-dropdown style="line-height: 50px;">
-        <span class="el-dropdown-link">
-          <span >{{ userName }}</span>
-          <el-icon class="el-icon--right">
-            <ArrowDown/>
-          </el-icon>
-        </span>
-        <template #dropdown>
-          <el-dropdown-menu>
-            <el-dropdown-item @click="gotoShoppingCart">我的购物车</el-dropdown-item>
-            <el-dropdown-item @click="showInfo">个人信息</el-dropdown-item>
-            <el-dropdown-item @click="gotoHome">首页</el-dropdown-item>
-            <el-dropdown-item @click="exitSystem">退出</el-dropdown-item>
-          </el-dropdown-menu>
-        </template>
-      </el-dropdown>
-    </div>
   </div>
 </template>
 
 <script>
-import { ArrowLeftBold } from '@element-plus/icons-vue'
-import { ArrowDown } from '@element-plus/icons-vue'
+import { NavBar } from 'vant';
 
 export default {
   name: "ReturnHeader",
   components: {
-    ArrowLeftBold,
-    ArrowDown
+    [NavBar.name]: NavBar,
   },
   data(){
     return{
